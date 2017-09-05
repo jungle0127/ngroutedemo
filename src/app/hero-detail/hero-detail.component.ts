4fd985a1c865f74339a34d5e007efc5f19eebedf
jungle0127 <arbor@foxmail.com>
2017-09-05 22:16:26 +0800

    update hero detail page

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { ActivatedRoute, ParamMap} from '@angular/router';
+import { Location} from '@angular/common';
+
+import { Hero} from '../hero';
+import { HeroService} from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -6,10 +12,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-
-  constructor() { }
+  hero: Hero;
+  constructor(
+    private heroService: HeroService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.route.paramMap
+    .switchMap((params: ParamMap) => this.heroService.getHero(+params.get('id')))
+    .subscribe(hero => this.hero = hero);
   }
-
+  goBack(): void {
+    this.location.back();
+  }
 }
